fb5605a938ee5c63364e4077259c5c873611fc2c
Guigso <gui.soliveira@hotmail.com>
2019-09-20 10:29:37 -0300

    build & start script

---
+++ dist/routes.js
@@ -0,0 +1,89 @@
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }Object.defineProperty(exports, "__esModule", {value: true});var _express = require('express');
+var _api = require('./services/api'); var _api2 = _interopRequireDefault(_api);
+
+const routes = new (0, _express.Router)();
+const base_img_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+function getDetails(detailObj) {
+  var filtered = {};
+  var objKeys = Object.keys(detailObj);
+
+  objKeys.forEach(ele => {
+    if (detailObj[ele] !== null && detailObj[ele] !== '' && ele !== 'turn_upside_down')
+      filtered[ele] = detailObj[ele];
+  });
+
+  return filtered;
+}
+routes.get('/:id', async (req, res) => {
+  var response = await _api2.default.get(`/generation/${req.params.id}`);
+
+  var pokemons = [];
+  response.data.pokemon_species.forEach(ele => {
+    pokemons.push({
+      id: ele.url.split('/').slice(6)[0],
+      name: ele.name,
+      sprite: base_img_url + ele.url.split('/').slice(6)[0] + '.png'
+    });
+  });
+
+  pokemons.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+
+  return res.json(pokemons);
+});
+
+routes.get('/details/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await _api2.default.get(`/pokemon/${id}`);
+    const specieResponse = await _api2.default.get(`/pokemon-species/${id}`);
+
+    if (specieResponse.data.evolution_chain.url) {
+      const evoResponse = await _api2.default.get(specieResponse.data.evolution_chain.url);
+      var evoChain = [];
+      var evoData = evoResponse.data.chain;
+      var evo_chain_level = 0;
+      do {
+        evo_chain_level++;
+        let numberOfEvolutions = evoData['evolves_to'].length;
+        var evoDetails = evoData['evolution_details'][0];
+
+        evoChain.push({
+          name: evoData.species.name,
+          id: evoData.species.url.split('/')[6],
+          sprite: base_img_url + evoData.species.url.split('/')[6] + '.png',
+          evo_chain_level
+        });
+
+        if (numberOfEvolutions > 1) {
+          for (let i = 1; i < numberOfEvolutions; i++) {
+            evoChain.push({
+              name: evoData.evolves_to[i].species.name,
+              id: evoData.evolves_to[i].species.url.split('/')[6],
+              sprite: base_img_url + evoData.species.url.split('/')[6] + '.png',
+              evolution_details: getDetails(evoData.evolves_to[i].evolution_details[0]),
+              evo_chain_level
+            });
+          }
+        }
+        evoData = evoData['evolves_to'][0];
+      } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
+      var evolutionChain = evoChain;
+    }
+    var pokemon = {
+      id: response.data.id,
+      name: response.data.name,
+      sprite: response.data.sprites.front_default,
+      types: response.data.types.reverse(),
+      evo_chain: evolutionChain
+    };
+
+    return res.json({
+      pokemon
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: err });
+  }
+});
+
+exports. default = routes;
